56e94d3b89a41916c5f2e37fcc10b3b294b72b20
Profe Juan <juanrinconada@cev.com>
2023-11-28 12:30:51 +0100

    Add POST request

--- lib/formulario.dart
+++ lib/formulario.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
+
 import 'package:ejemplo/listado.dart';
 import 'package:ejemplo/persona.dart';
 import 'package:ejemplo/textoTitulo.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class Formulario extends StatefulWidget {
   const Formulario({super.key, required this.title});
@@ -16,14 +19,25 @@ class _Formulario extends State<Formulario> {
   Persona persona = Persona('', 20);
 
   botonPulsado() {
-    Navigator.of(context)
-        .push(MaterialPageRoute(builder: (context) => Listado()));
+    postRegistro();    
   }
 
   edadCambiada(double edad) {
     setState(() {
       persona.edad = edad.round();
     });
+  }
+
+  Future<void> postRegistro() async {
+    Uri uri = Uri.parse('https://superapi.netlify.app/api/register');
+    final cabeceras = { 'Content-Type': 'application/json; charset=UTF-8' };
+    final cuerpoJSON = persona.toJSON();
+    final response = await http.post(uri, headers: cabeceras, body: cuerpoJSON);
+    
+    if (response.statusCode == 200) {
+      Navigator.of(context)
+        .push(MaterialPageRoute(builder: (context) => Listado()));
+    }    
   }
 
   @override
--- lib/persona.dart
+++ lib/persona.dart
@@ -1,7 +1,13 @@
 
+import 'dart:convert';
+
 class Persona {
   String nombre;
   int edad;
 
   Persona(this.nombre, this.edad);
+
+  String toJSON() {
+    return json.encode({"user" : nombre, "pass" : edad.toString()});
+  }
 }
